9a800f1eb6fdb83ecb7f204135385002a6d1dbe5
YusifAliyev037 <131958118+YusifAliyev037@users.noreply.github.com>
2024-02-11 22:07:33 +0400

    home page categorys

--- src/js/home-page.js
+++ src/js/home-page.js
@@ -52,10 +52,23 @@ function convertData(d) {
   return myNewData;
 }
 
+readData("/category")
+.then((data) =>{
+
+const categorys = Object.values(data)
+
+  console.log(categorys);
+  renderCatalog(categorys)
+})
+.catch((error) => console.error("Error reading data:", error));
 
 
-
-
+function renderCatalog (list){
+  linkGroup.innerHTML = list.map(item =>
+    ` <div class="form-link"><a href="#">${item}</a></div>
+                  
+    `).join("")
+}
 
 
 
