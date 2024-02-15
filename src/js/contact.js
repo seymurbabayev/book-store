87a04f1161428dc664c162a78d77a5f62ffa62af
nasiblihikmet <hikmetnesibli147@gmail.com>
2024-02-15 15:44:27 +0400

    contact-us connected  to firebase

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,3 +1,30 @@
+// Firebase konfiqurasiyası və tətbiqin başlatılması
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  set,
+  push,
+  onValue,
+  get,
+} from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js';
+const firebaseConfig = {
+    apiKey: "AIzaSyDGsqCFzK968Iw30ccw_sa63MJ71JH8Ask",
+    authDomain: "library-bookstore-47573.firebaseapp.com",
+    databaseURL: "https://library-bookstore-47573-default-rtdb.firebaseio.com",
+    projectId: "library-bookstore-47573",
+    storageBucket: "library-bookstore-47573.appspot.com",
+    messagingSenderId: "241881115117",
+    appId: "1:241881115117:web:87741476f3375fded59fe4",
+    measurementId: "G-TNGEJRZG18"
+  };
+
+  
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+const contacts = ref(database, "contacts");
 const send_Button = document.querySelector(".send_Button");
 let textLength = document.querySelector("#textLength");
 
@@ -22,7 +49,38 @@ const contactOb = {
      phone_Input,
     contactTextarea,
 };
+push(contacts,contactOb)
+document.querySelector(".name_Input").value = "";
+document.querySelector(".address_Input").value = "";
+document.querySelector(".email_Input").value = "";
+document.querySelector(".phone_Input").value = "";
+document.querySelector("#contactTextarea").value = "";
+showMessage("Successful process", false);
+textLength.textContent = "0";
 console.log("contactOb", contactOb);
 }
 
 })
+
+function letterCounter() {
+    var enteredText = contactTextarea.value;
+    var letterCount =enteredText.length;
+if (letterCount > 100) {
+contactTextarea.value = enteredText.substring(0, 100);
+letterCount = 100;
+}
+textLength.textContent = letterCount;
+}
+
+contactTextarea?.addEventListener("input", function () {
+    letterCounter();
+  });
+
+
+  
+
+
+  
+ 
+  
+  
